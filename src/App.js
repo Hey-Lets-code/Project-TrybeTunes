b0846ff0cae48fc1c1be4e6cf16d74f89102e63e
leticia <le.augusta.souza@gmail.com>
2022-08-30 17:23:38 -0300

    Requisito 1 - 30 de ago 2022 às 17:23

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Components/Login';
+import Search from './Components/Search';
+import Album from './Components/Album';
+import Favorite from './Components/Favorite';
+import Profile from './Components/Profile';
+import ProfileEdit from './Components/ProfileEdit';
+import NotFound from './Components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes </p>);
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorite } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
---
+++ src/Components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// export default class NotFound extends React.Component {}
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>Not Found</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
